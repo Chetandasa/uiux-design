b6a9e6d6896533b63220e6d19779c25a44d7a411
Your Name <chetandasa2814@gmail.com>
2023-05-03 23:59:55 +0530

    a

--- uiux/.eslintrc.js
+++ uiux/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: "latest",
     sourceType: 'module',
   },
   parser: "@babel/eslint-parser",
